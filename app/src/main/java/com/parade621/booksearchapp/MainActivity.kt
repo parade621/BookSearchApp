1a0fce3a2922391a601ab07a0372728fc99ee64e
parade621 <parade621@naver.com>
2022-12-28 20:27:36 +0900

    앱 초기 구조 작성
    ViewBinding, Fragment, bottom_navigation_view 적용

--- app/src/main/java/com/parade621/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/parade621/booksearchapp/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.parade621.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.parade621.booksearchapp.databinding.ActivityMainBinding
+import com.parade621.booksearchapp.ui.view.FavoriteFragment
+import com.parade621.booksearchapp.ui.view.SearchFragment
+import com.parade621.booksearchapp.ui.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+        // app이 처음 실행된 경우에만 searchFragment가 실행되게 한다.
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    /**
+     * Compose Bottom_Navigation_View
+     */
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/parade621/booksearchapp/ui/view/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.parade621.booksearchapp.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.parade621.booksearchapp.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
