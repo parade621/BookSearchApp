0cb1139b3556abdc5b092efaff910fe6468959f0
parade621 <parade621@naver.com>
2022-12-30 23:07:49 +0900

    Navigation으로 fragment 전환

--- app/src/main/java/com/parade621/booksearchapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/parade621/booksearchapp/ui/view/MainActivity.kt
@@ -3,6 +3,9 @@ package com.parade621.booksearchapp.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.parade621.booksearchapp.R
 import com.parade621.booksearchapp.data.repository.BookSearchRepositoryImpl
 import com.parade621.booksearchapp.databinding.ActivityMainBinding
@@ -16,16 +19,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupBottomNavigationView()
-        // app이 처음 실행된 경우에만 searchFragment가 실행되게 한다.
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
-        }
+//        setupBottomNavigationView()
+//        // app이 처음 실행된 경우에만 searchFragment가 실행되게 한다.
+//        if (savedInstanceState == null) {
+//            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+//        }
+
+        setupJetpackNavigation()
 
         val bookSearchRepository = BookSearchRepositoryImpl()
         val factory = BookSearchViewModelFactory(bookSearchRepository, this)
@@ -34,32 +40,41 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /**
-     * Compose Bottom_Navigation_View
-     */
-    private fun setupBottomNavigationView() {
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.fragment_search -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SearchFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_favorite -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, FavoriteFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_settings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SettingsFragment())
-                        .commit()
-                    true
-                }
-                else -> false
-            }
-        }
+    private fun setupJetpackNavigation() {
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.booksearch_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
+
+
+//    /**
+//     * Compose Bottom_Navigation_View
+//     */
+//    private fun setupBottomNavigationView() {
+//        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.fragment_search -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SearchFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_favorite -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, FavoriteFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_settings -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SettingsFragment())
+//                        .commit()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+//    }
+//}
